2710b3a033963be22c3995d3f8b86e647ae446c6
Amulya <ammukaranji8@gmail.com>
2026-07-31 22:12:14 +0530

    EDA and Problem Statement

--- src/exception.py
+++ src/exception.py
@@ -1,5 +1,5 @@
 import sys
-import logging
+from src.logger import logging
 
 def error_message_detail(error, error_detail: sys):
     _, _, exc_tb =error_detail.exc_info()    #This will give us the traceback object
@@ -15,3 +15,10 @@ class CustomException(Exception):
 
     def __str__(self):
         return self.error_message    #Whenever we print the object of this class, it will return the error message
+    
+'''if __name__ == "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info("Divide by zero error")
+        raise CustomException(e, sys)   #This will raise the custom exception with the error message and the traceback object'''
